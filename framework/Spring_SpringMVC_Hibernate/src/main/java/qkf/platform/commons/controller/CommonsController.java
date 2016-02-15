bf9bfb0ace364cf253244b84e2c2478f27b1196d
qkf <qkf5811@163.com>
2016-02-15 12:14:39 +0800

    比较完整的。。

---
+++ framework/Spring_SpringMVC_Hibernate/src/main/java/qkf/platform/commons/controller/CommonsController.java
@@ -0,0 +1,13 @@
+package qkf.platform.commons.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CommonsController {
+	
+	@RequestMapping("index")
+	public String indexPage() {
+		return "index";
+	}
+}
